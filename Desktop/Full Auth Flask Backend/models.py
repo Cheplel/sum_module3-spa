dfc6e9bfb87f5f20ec4da8a4229b3c6d930884b2
Jean <jean.koech@student.moringaschool.com>
2026-07-31 02:54:58 +0300

    Password hashing done

--- Desktop/Full Auth Flask Backend/models.py
+++ Desktop/Full Auth Flask Backend/models.py
@@ -1,4 +1,6 @@
-from turtle import title
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.ext.hybrid import hybrid_property
+
 
 
 class User(db.Model):
@@ -7,6 +9,32 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String, nullable = False)
     _password_hash = db.Column(db.String, nullable = False)
+
+#leaves all of the sqlalchemy characteristics of the column in place
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+
+#setter method for password property
+    @hybrid_property
+    def password_hash(self, password):
+        self._password_hash = self.simple_hash(password)
+
+#authentication method using user and password
+    def authenticate(self, password):
+        return self.simple_hash(password) == self._password_hash
+
+    @staticmethod
+    def simple_hash(input):
+        return sum(bytearray(input, encoding='utf-8'))
+
+
+    def set_password(self, password):
+        self._password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
     JournalEntries = db.Relationship ('JournalEntry' back_populate = 'user')
 
